fecfa888df78e7591406dfeab75574456cda1bd6
Au <sonienguyen1995@gmaill.com>
2025-11-25 03:57:35 +0000

    update

---
+++ LLaMA-Factory/scripts/8_infer_grpo.sh
@@ -0,0 +1,38 @@
+CUDA_VISIBLE_DEVICES=0 python3 scripts/vllm_infer.py \
+    --model_name_or_path /workspace/LLaMA-OSS/model/llama_grpo_merged \
+    --dataset sft_grpo_val_low \
+    --template llama3 \
+    --batch_size 4096 \
+    --default_system "" \
+    --cutoff_len 2048 \
+    --max_new_tokens 4096 \
+    --seed 42 \
+    --vllm_config '{"gpu_memory_utilization": 0.95, "max_model_len": 6144, "max_num_batched_tokens": 20480}' \
+    --save_name workspace/LLaMA-OSS/val/llama_grpo_low.jsonl \
+    --temperature 1.0 \
+
+CUDA_VISIBLE_DEVICES=0 python3 scripts/vllm_infer.py \
+    --model_name_or_path /workspace/LLaMA-OSS/model/llama_grpo_merged \
+    --dataset sft_grpo_val_medium \
+    --template llama3 \
+    --batch_size 4096 \
+    --default_system "" \
+    --cutoff_len 2048 \
+    --max_new_tokens 4096 \
+    --seed 42 \
+    --vllm_config '{"gpu_memory_utilization": 0.95, "max_model_len": 6144, "max_num_batched_tokens": 20480}' \
+    --save_name /workspace/LLaMA-OSS/val/llama_grpo_med.jsonl \
+    --temperature 1.0 \
+
+CUDA_VISIBLE_DEVICES=0 python3 scripts/vllm_infer.py \
+    --model_name_or_path /workspace/LLaMA-OSS/model/llama_grpo_merged \
+    --dataset sft_grpo_val_high \
+    --template llama3 \
+    --batch_size 4096 \
+    --default_system "" \
+    --cutoff_len 2048 \
+    --max_new_tokens 4096 \
+    --seed 42 \
+    --vllm_config '{"gpu_memory_utilization": 0.95, "max_model_len": 6144, "max_num_batched_tokens": 20480}' \
+    --save_name /workspace/LLaMA-OSS/val/llama_grpo_high.jsonl \
+    --temperature 1.0 \
