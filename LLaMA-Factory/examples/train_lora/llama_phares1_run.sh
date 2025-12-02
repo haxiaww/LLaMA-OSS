79bcf4a0534ae0410c57d2689b5bf7b2b49da085
Koii2k3 <devkoi2k3@gmail.com>
2025-12-03 03:37:13 +0700

    add run phase config

--- LLaMA-Factory/examples/train_lora/llama_phares1_run.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 llamafactory-cli train examples/train_lora/llama_pharse1.yaml
--- LLaMA-Factory/examples/train_lora/llama_phares2_run.sh
+++
@@ -1,2 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 llamafactory-cli train examples/train_lora/llama_pharse2.yaml
-CUDA_VISIBLE_DEVICES=1 llamafactory-cli train examples/train_lora/llama_pharse3.yaml
