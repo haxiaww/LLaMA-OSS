cb617d8ce022c10f4b635e10e311dcb4bce8af43
Koii2k3 <devkoi2k3@gmail.com>
2025-12-22 09:47:57 +0700

    setup new eval cònig

---
+++ LLaMA-Factory/lm-evaluation-harness/scripts/1_run_all_default.sh
@@ -0,0 +1,55 @@
+lm-evaluation-harness/scripts/1_eval_default.sh 2 output/final_llama_3b_sft_raw final_llama_raw
+lm-evaluation-harness/scripts/1_eval_default.sh 2 output/final_llama_3b_sft_origin final_llama_origin
+lm-evaluation-harness/scripts/1_eval_default.sh 2 output/final_llama_3b_sft_low final_llama_low
+lm-evaluation-harness/scripts/1_eval_default.sh 2 output/final_llama_3b_sft_medium final_llama_medium
+lm-evaluation-harness/scripts/1_eval_default.sh 2 output/final_llama_3b_sft_high final_llama_high
+lm-evaluation-harness/scripts/1_eval_default.sh 2 output/final_llama_3b_sft_low_high final_llama_lowhigh
+lm-evaluation-harness/scripts/1_eval_default.sh 2 output/final_llama_3b_sft_low_medium final_llama_lowmedium
+lm-evaluation-harness/scripts/1_eval_default.sh 2 output/final_llama_3b_sft_medium_high final_llama_mediumhigh
+lm-evaluation-harness/scripts/1_eval_default.sh 2 output/final_llama_3b_sft_low_medium_high final_llama_lowmediumhigh
+
+
+lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_llama_3b_sft_raw final_llama_raw
+lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_llama_3b_sft_origin final_llama_origin
+lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_llama_3b_sft_low final_llama_low
+lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_llama_3b_sft_medium final_llama_medium
+lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_llama_3b_sft_high final_llama_high
+lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_llama_3b_sft_low_high final_llama_lowhigh
+lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_llama_3b_sft_low_medium final_llama_lowmedium
+lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_llama_3b_sft_medium_high final_llama_mediumhigh
+lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_llama_3b_sft_low_medium_high final_llama_lowmediumhigh
+
+# lm-evaluation-harness/scripts/1_eval_default.sh 2 output/final_qwen_3b_sft_origin final_qwen_origin
+# lm-evaluation-harness/scripts/1_eval_default.sh 2 output/final_qwen_3b_sft_low final_qwen_low
+# lm-evaluation-harness/scripts/1_eval_default.sh 2 output/final_qwen_3b_sft_medium final_qwen_medium
+# lm-evaluation-harness/scripts/1_eval_default.sh 2 output/final_qwen_3b_sft_high final_qwen_high
+
+# lm-evaluation-harness/scripts/1_eval_default.sh 2 output/final_qwen_3b_sft_low_high final_qwen_lowhigh
+# lm-evaluation-harness/scripts/1_eval_default.sh 2 output/final_qwen_3b_sft_low_medium final_qwen_lowmedium
+# lm-evaluation-harness/scripts/1_eval_default.sh 2 output/final_qwen_3b_sft_medium_high final_qwen_mediumhigh
+# lm-evaluation-harness/scripts/1_eval_default.sh 2 output/final_qwen_3b_sft_low_medium_high final_qwen_lowmediumhigh
+
+
+
+
+# lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_llama_3b_sft_origin final_llama_origin
+# lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_llama_3b_sft_low final_llama_low
+# lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_llama_3b_sft_medium final_llama_medium
+# lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_llama_3b_sft_high final_llama_high
+
+# lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_llama_3b_sft_low_high final_llama_lowhigh
+# lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_llama_3b_sft_low_medium final_llama_lowmedium
+# lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_llama_3b_sft_medium_high final_llama_mediumhigh
+# lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_llama_3b_sft_low_medium_high final_llama_lowmediumhigh
+
+
+# lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_qwen_3b_sft_origin final_qwen_origin
+# lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_qwen_3b_sft_low final_qwen_low
+# lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_qwen_3b_sft_medium final_qwen_medium
+# lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_qwen_3b_sft_high final_qwen_high
+
+# lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_qwen_3b_sft_low_high final_qwen_lowhigh
+# lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_qwen_3b_sft_low_medium final_qwen_lowmedium
+# lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_qwen_3b_sft_medium_high final_qwen_mediumhigh
+# lm-evaluation-harness/scripts/1_eval_0shot.sh 2 output/final_qwen_3b_sft_low_medium_high final_qwen_lowmediumhigh
+
